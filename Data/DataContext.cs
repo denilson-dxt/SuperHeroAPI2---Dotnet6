bbea69800ac4dc1b98d5dfdd7308fc3aa78f3574
Denilson Tivane <denilsontivane@gmail.com>
2022-05-22 13:02:49 +0200

    First version project

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI2.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext (DbContextOptions<DataContext> options): base(options) {}
+    
+    public DbSet<SuperHero> SuperHeroes { get; set; }
+}
