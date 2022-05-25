3f257c75e8f2ab86179f2861d4bbe30940001305
Denilson Tivane <denilsontivane@gmail.com>
2022-05-25 15:30:48 +0200

    Migrate to mysql

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,10 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace SuperHeroAPI2.Data;
-
+using Microsoft.EntityFrameworkCore;
 public class DataContext : DbContext
 {
-    public DataContext (DbContextOptions<DataContext> options): base(options) {}
-    
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+
     public DbSet<SuperHero> SuperHeroes { get; set; }
 }
